031373ed9778169bd3eb5ae22da530b2085cf80a
MuratYigitMert <yigit.mert@ozu.edu.tr>
2024-12-12 15:11:22 +0300

    123

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    // Database URL, username, and password
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/hotel_management12";
+    private static final String USER = "root"; //
+    private static final String PASSWORD = "2023"; //
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Load and register MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish the connection
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+
+}
